de4416bf3e82f5cd69b179aa239b93353ccf05ab
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-07-14 15:36:19 +0530

    Added: Creating Board

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,7 +1,17 @@
 package com.bridgelabz.tictactoe;
 
 public class TicTacToeGame {
+
+    static char board[]=new char[10];
+
+    static void createBoard(char[]board){
+        for(int i=1; i<10; i++){
+            board[i]=' ';
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to TicTacToe Game");
+        createBoard(board);
     }
+
 }
